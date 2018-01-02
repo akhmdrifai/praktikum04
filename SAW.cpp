95993a33d7776b01d046541f630bd7f84c0af06b
akhmdrifai <32448266+akhmdrifai@users.noreply.github.com>
2018-01-02 16:08:57 +0800

    hasil dari SAW.cpp

--- SAW.cpp
+++ SAW.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int main(){
-int main(){
 	
 	double a1c[5]={4,4,5,3,3};
 	double a2c[5]={3,3,4,2,3};
@@ -127,7 +126,7 @@ int main(){
 	
 	ha1=RWa1c[0]+RWa1c[1]+RWa1c[2]+RWa1c[3]+RWa1c[4];
 	ha2=RWa2c[0]+RWa2c[1]+RWa2c[2]+RWa2c[3]+RWa2c[4];
-	ha3=RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4]
+	ha3=RWa3c[0]+RWa3c[1]+RWa3c[2]+RWa3c[3]+RWa3c[4];
 
 // MENAMPILKAN NILAI
 	cout << "MATRIKS X :"<<endl;
